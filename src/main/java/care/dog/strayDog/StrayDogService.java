5deeac73c71ac27d94efe5d755a508d94b9c6b6b
hshs <chlgptmd123@gmail.com>
2018-03-08 17:26:20 +0900

    유기견

--- src/main/java/care/dog/strayDog/StrayDogService.java
+++ src/main/java/care/dog/strayDog/StrayDogService.java
@@ -2,9 +2,14 @@ package care.dog.strayDog;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public interface StrayDogService {
-	public Map<String, Object> listSido();
-	public ArrayList<HashMap<String, Object>> strayDog();
+	public List<Map<String, Object>> listSido(); //시도 리스트 호출
+	public List<Map<String, Object>> listGu(String sido); //구 리스트 호출
+	public List<Map<String, Object>> listCenter(String sido, String gu); //구 리스트 호출
+	public List<Map<String, Object>> listKind(); //종 리스트 호출
+	
+	public ArrayList<HashMap<String, Object>> strayDog(Map<String, Object> model); //유기견 리스트 호출
 }
