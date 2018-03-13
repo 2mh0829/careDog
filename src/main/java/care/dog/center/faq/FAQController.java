b257570c07410d900899dfbfb06b306699500e47
eunhyeong <keh94k@gmail.com>
2018-03-13 15:30:25 +0900

    커밋

--- src/main/java/care/dog/center/faq/FAQController.java
+++ src/main/java/care/dog/center/faq/FAQController.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("faq.faqController")
-public class FaqController {
+public class FAQController {
 	
 	@RequestMapping(value="/center/faq")
 	public String faqList() {
-		return ".center.faq";
+		return "center/faq";
 	}
+	
+	
 	
 }
