a5b29658b060752ab7b867134de63e94a0e8999d
이명하 <emh0829@gmail.com>
2018-03-14 12:18:24 +0900

    인증 & 세션 종료

--- src/main/java/care/dog/member/mail/MailController.java
+++ src/main/java/care/dog/member/mail/MailController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller("mail.mailController")
 public class MailController {
@@ -11,15 +12,16 @@ public class MailController {
 	private MailSender mailSender;
 
 	@RequestMapping(value="/member/join3")
-	public String sendSubmit(Mail dto, Model model, String email1, String email2) throws Exception {
+	public String sendSubmit(Mail dto, Model model, String email1, String email2, String userId) throws Exception {
 
 		dto.setReceiverEmail(email1 + "@" + email2);
+		int userHashCode = userId.hashCode();
 		
 		String content = "";
-		content += "<div align='center' style='background: #eee; margin-top: 30px; padding: 20px;'>";
+		content += "<div align='center' style='background: #eee; margin-top: 30px; padding: 20px; width: 800px;'>";
 		content += "<div style='font-size: 30px; font-weight: bold; margin: 30px auto;'>Care Dog 가입을 환영합니다.</div>";
 		content += "<img src='http://cfile23.uf.tistory.com/image/9931294B5AA7E1542BB6B8' width='400px;'>";
-		content += "<br><a href='http://localhost:9090/careDog/member/join3' target='_blank'>";
+		content += "<br><a href='http://localhost:9090/careDog/member/auth?authCode=\"" + userHashCode + "\"' target='_blank'>";
 		content += "<button style='background: #4286f4; width:300px; height: 50px; color: white; font-size: 20px; font-weight: bold; margin: 30px auto;' type='button'>인증하기</button></a></div>";
 		
 		dto.setContent(content);
@@ -37,4 +39,24 @@ public class MailController {
 		
 		return ".member.join3";
 	}
+	
+	@RequestMapping(value="/member/auth")
+	public String auth(@RequestParam int authCode) {
+		
+		System.out.println(authCode);
+		
+		// 인증
+		
+		return "member/auth";
+	}
+
+	@RequestMapping(value="/member/auth_completed")
+	public void auth_completed() {
+		
+		// 삭제
+		System.out.println("completed");
+		
+	}
+	
+	
 }
