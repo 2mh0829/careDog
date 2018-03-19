b77bff868f2ec70e0131f55fd5e8f247804fa18e
eunhyeong <keh94k@gmail.com>
2018-03-19 09:22:48 +0900

    아침커밋

---
+++ src/main/java/care/dog/center/onefone/OneFOneController.java
@@ -0,0 +1,15 @@
+package care.dog.center.onefone;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller("onefone.onefoneController")
+public class OneFOneController {
+	
+	@RequestMapping(value="/center/onefone/create", method=RequestMethod.GET)
+	public String OneforOneMain() {
+		return "";
+	}
+
+}
